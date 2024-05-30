8bc39c0e996d8b4c7293697e9c157284455823d5
EventHorizontal <doubleedgedsword112@gmail.com>
2024-05-31 00:24:21 +0600

    Added the self-damage part of Life Orb's effect.

--- src/item_dex.rs
+++ src/item_dex.rs
@@ -1,5 +1,6 @@
 #![allow(non_upper_case_globals, clippy::zero_prefixed_literal, unused)]
 
+use monsim::{effects, MoveUseContext};
 use monsim_utils::Percent;
 
 use crate::{item::{ItemDexData, ItemKind, ItemSpecies}, source_code_location, EventFilteringOptions, EventHandler, EventHandlerDeck, TargetFlags};
@@ -15,9 +16,18 @@ pub const LifeOrb: ItemSpecies = ItemSpecies::from_dex_data(
                 on_modify_damage: Some(EventHandler {
                     #[cfg(feature = "debug")]
                     source_code_location: source_code_location!(),
-                    response: |_sim, _broadcaster_id, _receiver_id, _, damage| {
-                        _sim.push_message("Life orb boost!");
+                    response: |sim, broadcaster_id, _receiver_id, _, damage| {
+                        sim.push_message(format!["Life orb boosted the damage of {}'s attack!", sim.battle.monster(broadcaster_id).name()]);
                         damage * Percent(130)
+                    },
+                }),
+                on_move_used: Some(EventHandler {
+                    #[cfg(feature = "debug")]
+                    source_code_location: source_code_location!(),
+                    response: |sim, broadcaster_id, receiver_id, MoveUseContext { move_user_id, move_used_id, target_ids }, _| {
+                        let one_tenth_of_total_hp = sim.battle.monster(move_user_id).max_health() * Percent(10);
+                        sim.push_message(format!["Life orb drained some of {}'s life force!", sim.battle.monster(broadcaster_id).name()]);
+                        let damage_dealt = effects::deal_raw_damage(sim, (move_user_id, one_tenth_of_total_hp));
                     },
                 }),
                 ..EventHandlerDeck::empty()

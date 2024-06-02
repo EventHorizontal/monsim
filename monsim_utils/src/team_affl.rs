def2602e7f16f8c84d3023afccfdc0478e85af07
EventHorizontal <doubleedgedsword112@gmail.com>
2024-06-02 23:06:39 +0600

    Seperated the team affiliation related stuff into its own module.

---
+++ monsim_utils/src/team_affl.rs
@@ -0,0 +1,204 @@
+use std::ops::{Deref, DerefMut};
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+/// A type that can be deferenced to get data marked as belonging to the Ally Team
+pub struct Ally<T>(T);
+
+impl<T> Deref for Ally<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        & self.0
+    }
+}
+
+impl<T> DerefMut for Ally<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl<T> AsRef<T> for Ally<T> {
+    fn as_ref(&self) -> &T {
+        &self.0
+    }
+}
+
+impl<T> AsMut<T> for Ally<T> {
+    fn as_mut(&mut self) -> &mut T {
+        &mut self.0
+    }
+}
+
+impl<T> Into<TeamAffl<T>> for Ally<T> {
+    fn into(self) -> TeamAffl<T> {
+        TeamAffl::Ally(self)
+    }
+}
+
+impl<T> Ally<T> {
+    pub const fn new(item: T) -> Self {
+        Self(item)
+    }
+
+    pub fn unwrap(self) -> T {
+        self.0
+    }
+
+    pub fn map_consume<U, F>(self, f: F) -> Ally<U> where F: FnOnce(T) -> U {
+        let item = f(self.0);
+        Ally(item)
+    }
+}
+
+pub trait IntoAlly<T> {
+    fn mark_as_ally(self) -> Ally<T>;
+}
+
+impl<T> IntoAlly<T> for T {
+    fn mark_as_ally(self) -> Ally<T> {
+        Ally::new(self)
+    }
+}
+
+impl<T: Clone> Ally<T> {
+    pub fn map_clone<U, F>(&self, f: F) -> Ally<U> where F: FnOnce(T) -> U {
+        let item = f(self.0.clone());
+        Ally(item)
+    }
+}
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+/// A type that can be deferenced to get data marked as belonging to the Opponent Team
+pub struct Opponent<T>(T);
+
+impl<T> Deref for Opponent<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        & self.0
+    }
+}
+
+impl<T> DerefMut for Opponent<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl<T> AsRef<T> for Opponent<T> {
+    fn as_ref(&self) -> &T {
+        &self.0
+    }
+}
+
+impl<T> AsMut<T> for Opponent<T> {
+    fn as_mut(&mut self) -> &mut T {
+        &mut self.0
+    }
+}
+
+impl<T> Into<TeamAffl<T>> for Opponent<T> {
+    fn into(self) -> TeamAffl<T> {
+        TeamAffl::Opponent(self)
+    }
+}
+
+impl<T> Opponent<T> {
+    pub const fn new(item: T) -> Self {
+        Self(item)
+    }
+
+    pub fn unwrap(self) -> T {
+        self.0
+    }
+
+    pub fn map_consume<U, F>(self, f: F) -> Opponent<U> 
+        where F: FnOnce(T) -> U 
+    {
+        let item = f(self.0);
+        Opponent(item)
+    }
+}
+
+impl<T: Clone> Opponent<T> {
+    pub fn map_clone<U, F>(&self, f: F) -> Opponent<U> where F: FnOnce(T) -> U {
+        let item = f(self.0.clone());
+        Opponent(item)
+    }
+}
+
+pub trait IntoOpponent<T> {
+    fn mark_as_opponent(self) -> Opponent<T>;
+}
+
+impl<T> IntoOpponent<T> for T {
+    fn mark_as_opponent(self) -> Opponent<T> {
+        Opponent::new(self)
+    }
+}
+
+pub enum TeamAffl<T> {
+    Ally(Ally<T>),
+    Opponent(Opponent<T>)
+}
+
+impl<T> TeamAffl<T> {
+    pub fn ally(item: Ally<T>) -> Self {
+        Self::Ally(item)
+    }
+
+    pub fn opponent(item: Opponent<T>) -> Self {
+        Self::Opponent(item)
+    }
+    
+    pub fn apply<U, F>(&self, f: F) -> U where F: FnOnce(&T) -> U {
+        match self {
+            TeamAffl::Ally(a) => f(&**a),
+            TeamAffl::Opponent(o) => f(&**o),
+        }
+    }
+
+    pub fn expect_ally(self) -> Ally<T> {
+        match self {
+            TeamAffl::Ally(a) => a,
+            TeamAffl::Opponent(_) => panic!(),
+        }
+    }
+
+    pub fn expect_opponent(self) -> Opponent<T> {
+        match self {
+            TeamAffl::Ally(_) => panic!(),
+            TeamAffl::Opponent(o) => o,
+        }
+    }
+    
+    pub fn unwrap(self) -> T {
+        match self {
+            TeamAffl::Ally(a) => a.0,
+            TeamAffl::Opponent(o) => o.0,
+        }
+    }
+}
+
+impl<T> TeamAffl<T> {
+    pub fn map<U, F>(self, f: F) -> TeamAffl<U> 
+        where F: FnOnce(T) -> U
+    {
+        match self {
+            TeamAffl::Ally(a) => TeamAffl::Ally(a.map_consume(f)),
+            TeamAffl::Opponent(o) => TeamAffl::Opponent(o.map_consume(f)),
+        }
+    }
+}
+
+impl<T: Clone> Deref for TeamAffl<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        match self {
+            TeamAffl::Ally(a) => &a,
+            TeamAffl::Opponent(o) => &o,
+        }
+    }
+}

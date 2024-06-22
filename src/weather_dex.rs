af6ed14a8964e6f3525bb23309546176cfbf8c01
EventHorizontal <doubleedgedsword112@gmail.com>
2024-06-23 00:07:17 +0600

    Fixed a small bug in the way Harsh Sunlight was rewritten.

--- src/weather_dex.rs
+++ src/weather_dex.rs
@@ -13,15 +13,15 @@ pub const HarshSunlight: WeatherSpecies = WeatherSpecies::from_dex_entry(Weather
     dex_number: 001,
     name: "Harsh Sunlight",
     lifetime_in_turns: Count::Fixed(5),
-    event_listener: &NullEventListener,
+    event_listener: &HarshSunlightEventListener,
     on_start_message: "The sunlight became harsh!",
     on_clear_message: "The harsh sunlight faded!",
 });
 
 struct HarshSunlightEventListener;
 
-impl EventListener<()> for HarshSunlightEventListener {
-    fn on_modify_damage_handler(&self) -> Option<EventHandler<MoveHitContext, u16, Nothing, MonsterID>> {
+impl EventListener<Nothing, Nothing> for HarshSunlightEventListener {
+    fn on_modify_damage_handler(&self) -> Option<EventHandler<MoveHitContext, u16, Nothing, Nothing>> {
         Some(EventHandler {
             response: |battle, broadcaster_id, receiver_id, _, context, damage| {
                 if mov![context.move_used_id].is_type(Type::Fire) {
@@ -43,7 +43,7 @@ impl EventListener<()> for HarshSunlightEventListener {
         })
     }
 
-    fn on_turn_end_handler(&self) -> Option<EventHandler<Nothing, Nothing, Nothing, MonsterID, Nothing>> {
+    fn on_turn_end_handler(&self) -> Option<EventHandler<Nothing, Nothing, Nothing, Nothing, Nothing>> {
         Some(EventHandler {
             response: |battle, _, _, _, _, _| {
                 battle.queue_message("The sunlight remains strong.");
